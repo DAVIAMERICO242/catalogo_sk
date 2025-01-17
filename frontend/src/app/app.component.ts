0a9f9395352e28c64752d5b3ed4db37560c77a2d
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-17 14:46:18 -0300

    OI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import { MyTheme } from './app.config';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'catalogo-sk';
+
+  constructor(private primeng: PrimeNG){
+
+  }
+  ngOnInit(): void {
+    this.setTheme();
+    this.setApplicationLanguage();
+  }
+
+  setTheme(){
+    this.primeng.theme.set({
+      preset: MyTheme
+    });
+  }
+
+  setApplicationLanguage(){
+    const monthNames = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'];
+    const monthNamesShort = monthNames.map((e)=>e.slice(0,3));
+    const dayNames = ['Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Doming'];
+    const dayNamesShort = dayNames.map(e=>e.slice(0,3))
+    const dayNamesMin = dayNamesShort;
+    this.primeng.setTranslation({
+      monthNames:monthNames,
+      monthNamesShort: monthNamesShort,
+      dayNames:dayNames,
+      dayNamesShort:dayNamesShort,
+      dayNamesMin:dayNamesMin
+    })
+  }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,30 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { definePreset } from '@primeng/themes';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './jwt.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+
+export const MyTheme = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{slate.100}',
+          100: '{slate.900}',
+          200: '{slate.900}',
+          300: '{slate.900}',
+          400: '{slate.900}',
+          500: '{slate.900}',
+          600: '{slate.900}',
+          700: '{slate.900}',
+          800: '{slate.900}',
+          900: '{slate.900}',
+          950: '{slate.900}'
+      }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: 
