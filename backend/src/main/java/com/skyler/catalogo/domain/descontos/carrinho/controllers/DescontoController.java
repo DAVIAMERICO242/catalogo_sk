e39d65c1bc8953857a9c150853b11de66007712e
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-24 10:14:43 -0300

    OI

--- backend/src/main/java/com/skyler/catalogo/domain/descontos/carrinho/controllers/DescontoController.java
+++ backend/src/main/java/com/skyler/catalogo/domain/descontos/carrinho/controllers/DescontoController.java
@@ -1,6 +1,8 @@
 package com.skyler.catalogo.domain.descontos.carrinho.controllers;
 
+import com.skyler.catalogo.domain.descontos.carrinho.DTOs.DescontoDTO;
 import com.skyler.catalogo.domain.descontos.carrinho.services.DescontoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,21 +16,21 @@ public class DescontoController {
     }
 
 
-//    @GetMapping("/nivel-loja")
-//    public ResponseEntity getNivelLoja(String lojaId){
-//        try{
-//            return ResponseEntity.ok().body(this.descontoCarrinhoService.getDescontos(lojaId));
-//        }catch (Exception e){
-//            return ResponseEntity.status(500).body(e.getLocalizedMessage());
-//        }
-//    }
-//
-//    @PostMapping("/nivel-loja")
-//    public ResponseEntity criarDescontoLoja(@RequestBody DescontoCarrinhoDTO payload){
-//        try{
-//            return ResponseEntity.ok().body(this.descontoCarrinhoService.criarAtualizarDesconto(payload));
-//        }catch (Exception e){
-//            return ResponseEntity.status(500).body(e.getLocalizedMessage());
-//        }
-//    }
+    @GetMapping
+    public ResponseEntity getDescontos(String lojaId){
+        try{
+            return ResponseEntity.ok().body(this.descontoService.getDescontosForLoja(lojaId));
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(e.getLocalizedMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity criarAtualizarDesconto(@RequestBody DescontoDTO descontoDTO){
+        try{
+            return ResponseEntity.ok().body(this.descontoService.cadastrarAtualizarDesconto(descontoDTO));
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(e.getLocalizedMessage());
+        }
+    }
 }
