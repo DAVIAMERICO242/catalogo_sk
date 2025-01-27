55e63e70f8e066d140e5fb6c22b7e896b1b35e95
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-27 17:14:39 -0300

    OI

--- backend/src/main/java/com/skyler/catalogo/infra/storage/MinioService.java
+++ backend/src/main/java/com/skyler/catalogo/infra/storage/MinioService.java
@@ -3,6 +3,7 @@ package com.skyler.catalogo.infra.storage;
 import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Item;
+import jakarta.validation.constraints.Min;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
@@ -10,22 +11,24 @@ import org.springframework.stereotype.Service;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 
 @Service
 public class MinioService implements ApplicationListener<ApplicationReadyEvent> {
 
-    private final MinioClient minioClient;
+    private final MinioClient authenticatedMinioClient;
     private final String bucket = "catalogosk";
     private final String minioUrl = "https://s3.skyler.com.br";
 
     public MinioService() {
-        this.minioClient =
+        this.authenticatedMinioClient =
                 MinioClient.builder()
                 .endpoint(this.minioUrl)
                 .credentials("vQ97tRxPkrkslrk6jkrR","00pIhKHvk0pblr2k1gL26vPUSx7bZeyqnA5RgB4M")
-                .build();;
+                .build();
     }
 
     @Override
@@ -33,39 +36,37 @@ public class MinioService implements ApplicationListener<ApplicationReadyEvent> 
 
         String object = "eu.png";
         try {
-            minioClient.removeObject(
+            authenticatedMinioClient.removeObject(
                     RemoveObjectArgs.builder()
                             .bucket(bucket)
                             .object(object)
                             .build()
             );
-            this.postBase64();
-            this.printFiles();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void postBase64() throws Exception{
-        String file = Base64StringTest.base64;
-        byte[] decoded = Base64.getDecoder().decode(file);
+    public void postBase64(String base64, String fileNameWithExtension, String pathFromBucketStartWithoutPointAndSlash) throws Exception{
+        byte[] decoded = Base64.getDecoder().decode(base64);
         InputStream inputStream = new java.io.ByteArrayInputStream(decoded);
-        minioClient.putObject(
+        authenticatedMinioClient.putObject(
                 PutObjectArgs
                         .builder()
                         .bucket(bucket)
-                        .object("loja/Nome.png")
+                        .object(pathFromBucketStartWithoutPointAndSlash + fileNameWithExtension)
                         .stream(inputStream, decoded.length,-1)
                         .build()
         );
     }
 
-    public void printFiles() throws Exception{
-        Iterable<Result<Item>> objects = minioClient.listObjects(
+    public List<String> getFileUrls(String pathFromBucketStartWithoutPointAndSlash) throws Exception{
+        List<String> output = new ArrayList<>();
+        Iterable<Result<Item>> objects = authenticatedMinioClient.listObjects(
                 ListObjectsArgs
                         .builder()
                         .bucket(bucket)
-                        .prefix("loja/")
+                        .prefix(pathFromBucketStartWithoutPointAndSlash)
                         .build()
         );
         for (Result<Item> result: objects) {
@@ -76,11 +77,13 @@ public class MinioService implements ApplicationListener<ApplicationReadyEvent> 
 
                 // Gerar a URL pública do arquivo
                 String fileUrl = this.minioUrl + "/" + bucket + "/" + item.objectName();
+                output.add(fileUrl);
 
                 // Imprimir a URL do arquivo
                 System.out.println("URL do arquivo: " + fileUrl.toString());
             }
         }
+        return output;
     }
 
     public String encodeToBase64(InputStream inputStream) throws Exception {
