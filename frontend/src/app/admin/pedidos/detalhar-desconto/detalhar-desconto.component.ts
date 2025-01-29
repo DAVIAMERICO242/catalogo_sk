b21502dc43450175edd8662519b72021c56389f1
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-29 17:25:00 -0300

    OI

--- frontend/src/app/admin/pedidos/detalhar-desconto/detalhar-desconto.component.ts
+++ frontend/src/app/admin/pedidos/detalhar-desconto/detalhar-desconto.component.ts
@@ -16,4 +16,8 @@ export class DetalharDescontoComponent {
   getTotalDescontos(val:Pedidos.DescontoAplicado[]){
     return val.reduce((a,b)=>a+b.valorAplicado,0)
   }
+
+  forceType(val:any){
+    return val as Pedidos.DescontoAplicado;
+  }
 }
