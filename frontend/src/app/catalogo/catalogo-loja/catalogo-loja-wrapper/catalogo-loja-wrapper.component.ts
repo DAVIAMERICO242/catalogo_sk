f5f9a0bbb4d40416d92dc319328123b27a22eee1
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-30 17:37:44 -0300

    OI

---
+++ frontend/src/app/catalogo/catalogo-loja/catalogo-loja-wrapper/catalogo-loja-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-loja-wrapper',
+  imports: [],
+  templateUrl: './catalogo-loja-wrapper.component.html'
+})
+export class CatalogoLojaWrapperComponent {
+
+}
--- frontend/src/app/catalogo/catalogo-loja/catalogo-loja.component.ts
+++ frontend/src/app/catalogo/catalogo-loja/catalogo-loja.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CatalogoService } from '../../services/catalogo.service';
+import { Catalogo, CatalogoService } from '../../services/catalogo.service';
 import { CatalogoHeaderComponent } from "./catalogo-header/catalogo-header.component";
 import { CatalogoContextService } from './catalogo-context.service';
+import { Observable } from 'rxjs';
+import { SharedModule } from '../../shared/shared.module';
+import { CatalogoLojaWrapperComponent } from "./catalogo-loja-wrapper/catalogo-loja-wrapper.component";
 
 @Component({
   selector: 'app-catalogo-loja',
-  imports: [CatalogoHeaderComponent],
+  imports: [CatalogoHeaderComponent, SharedModule, CatalogoLojaWrapperComponent],
   templateUrl: './catalogo-loja.component.html',
   providers:[CatalogoContextService]
 })
 export class CatalogoLojaComponent implements OnInit {
   slug = "";
+  produtos$!:Observable<Catalogo.Produto[] | undefined>;
+
 
   constructor(private route:ActivatedRoute,private catalogContext:CatalogoContextService){
-
   }
   ngOnInit(): void {
     this.slug = this.route.snapshot.paramMap.get("slug") || ""
     console.log(this.slug)
     this.loadContext();
+    this.produtos$ = this.catalogContext.produtos$;
   }
 
   loadContext(){
