1b308773bf5843a910c4ffcbd5b562584cabfdb2
INTELIGENCIA <darodriguesufc@gmail.com>
2025-01-31 17:27:15 -0300

    OI

--- frontend/src/app/admin/banners/banners.component.ts
+++ frontend/src/app/admin/banners/banners.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdminPageTitleComponent } from "../admin-page-title/admin-page-title.component";
+import { User, UserService } from '../../services/user.service';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-banners',
-  imports: [AdminPageTitleComponent],
+  imports: [AdminPageTitleComponent,SharedModule],
   templateUrl: './banners.component.html'
 })
-export class BannersComponent {
-
+export class BannersComponent implements OnInit {
+  lojas!:User.Loja[]
+  constructor(private auth:UserService){}
+  ngOnInit(): void {
+    if(this.auth.getContext()?.role===User.Role.ADMIN){
+      const lojas = this.auth.getContext()?.lojasFranquia
+      if(lojas){
+        this.lojas = lojas;
+      }
+    }else{
+      const loja = this.auth.getContext()?.loja;
+      if(loja){
+        this.lojas = [loja]
+      }
+    }
+  }
 }
