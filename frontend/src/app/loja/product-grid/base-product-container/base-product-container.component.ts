3c4d9f1fa8f796890b77a0807008375a1e0edc42
INTELIGENCIA <darodriguesufc@gmail.com>
2025-02-05 15:25:56 -0300

    OI

--- frontend/src/app/loja/product-grid/base-product-container/base-product-container.component.ts
+++ frontend/src/app/loja/product-grid/base-product-container/base-product-container.component.ts
@@ -3,20 +3,24 @@ import { Component, Input } from '@angular/core';
 import { Produto } from '../../../services/produtos.service';
 import { Catalogo } from '../../../services/catalogo.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-base-product-container',
-  imports: [CommonModule],
+  imports: [SharedModule],
   templateUrl: './base-product-container.component.html'
 })
 export class BaseProductContainerComponent {
   @Input({required:true})
-  produto!:Catalogo.Produto;
+  produto!:Catalogo.Produto | undefined;
+
 
   constructor(private router:Router,private route: ActivatedRoute) {}
   
 
   goToProductPage(){
-    this.router.navigate(["produto-catalogo/" + this.produto.systemId],{relativeTo:this.route})
+    if(this.produto){
+      this.router.navigate(["produto-catalogo/" + this.produto.systemId],{relativeTo:this.route})
+    }
   }
 }
