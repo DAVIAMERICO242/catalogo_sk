250810d73042e6a9589d89a007a7dcdc5e7dbdb8
INTELIGENCIA <darodriguesufc@gmail.com>
2025-02-12 13:35:31 -0300

    OI

--- backend/src/main/java/com/skyler/catalogo/domain/shipping/ShippingCalculator.java
+++ backend/src/main/java/com/skyler/catalogo/domain/shipping/ShippingCalculator.java
@@ -1,6 +1,10 @@
 package com.skyler.catalogo.domain.shipping;
 
 
+import com.skyler.catalogo.domain.descontos.interfaces.Discountable;
+import com.skyler.catalogo.domain.descontos.services.DiscountCalculator;
+import com.skyler.catalogo.domain.lojas.Loja;
+import com.skyler.catalogo.domain.pedidos.DTOs.LojaPedidoDTO;
 import com.skyler.catalogo.domain.pedidos.DTOs.ProdutoPedidoDTO;
 import com.skyler.catalogo.domain.produtos.entities.ProdutoVariacao;
 import com.skyler.catalogo.domain.produtos.repositories.ProdutoVariacaoRepository;
@@ -13,8 +17,12 @@ import com.skyler.catalogo.domain.shipping.correios.apiCorreios.CorreiosBridge;
 import com.skyler.catalogo.domain.franquias.Franquia;
 import com.skyler.catalogo.domain.lojas.LojaRepository;
 import com.skyler.catalogo.domain.pedidos.DTOs.PedidoBeforeCalculationsDTO;
+import com.skyler.catalogo.domain.shipping.faixasCep.ShippingRules;
+import com.skyler.catalogo.domain.shipping.faixasCep.ShippingRulesRepository;
 import com.skyler.catalogo.domain.shipping.pesoCategorias.PesoCategorias;
 import com.skyler.catalogo.domain.shipping.pesoCategorias.PesoCategoriasRepository;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -32,8 +40,10 @@ public class ShippingCalculator {
     private final ComprimentoCaixaRepository comprimentoCaixaRepository;
     private final PesoCategoriasRepository pesoCategoriasRepository;
     private final ProdutoVariacaoRepository produtoVariacaoRepository;
+    private final ShippingRulesRepository faixaCepRepository;
+    private final DiscountCalculator discountCalculator;
 
-    public ShippingCalculator(CorreiosFranquiaRepository correiosFranquiaRepository, CorreiosBridge correiosBridge, CorreiosAuth correiosAuth, LojaRepository lojaRepository, ComprimentoCaixaRepository comprimentoCaixaRepository, PesoCategoriasRepository pesoCategoriasRepository, ProdutoVariacaoRepository produtoVariacaoRepository) {
+    public ShippingCalculator(CorreiosFranquiaRepository correiosFranquiaRepository, CorreiosBridge correiosBridge, CorreiosAuth correiosAuth, LojaRepository lojaRepository, ComprimentoCaixaRepository comprimentoCaixaRepository, PesoCategoriasRepository pesoCategoriasRepository, ProdutoVariacaoRepository produtoVariacaoRepository, ShippingRulesRepository faixaCepRepository, DiscountCalculator discountCalculator) {
         this.correiosFranquiaRepository = correiosFranquiaRepository;
         this.correiosBridge = correiosBridge;
         this.correiosAuth = correiosAuth;
@@ -41,14 +51,43 @@ public class ShippingCalculator {
         this.comprimentoCaixaRepository = comprimentoCaixaRepository;
         this.pesoCategoriasRepository = pesoCategoriasRepository;
         this.produtoVariacaoRepository = produtoVariacaoRepository;
+        this.faixaCepRepository = faixaCepRepository;
+        this.discountCalculator = discountCalculator;
     }
 
+
+    public FreteResponseDTO getFrete(ShippingCalculationRequest shippingCalculationRequest){//precisa do valor maturo aaaa
+        Loja loja = this.lojaRepository.findById(shippingCalculationRequest.getLojaId()).get();
+        Franquia franquia = loja.getFranquia();
+        List<ShippingRules> faixasCep = this.faixaCepRepository.findAllByFranquiaId(franquia.getSystemId());
+        List<ShippingRules> faixasCepPrecificadas = faixasCep.stream().filter(o->o.getMinValueToApply()>0).toList();
+        List<ShippingRules> faixasCepNaoPrecificadas = faixasCep.stream().filter(o->o.getMinValueToApply()>0).toList();
+        ShippingDiscountable shippingDiscountable = this.mapToDiscountable(shippingCalculationRequest,loja);
+        Float valorCarrinhoComDescontos = this.discountCalculator.processChainForCurrentEpochAndDiscountable(shippingDiscountable).getValorFinal();
+        for(ShippingRules faixaCepPrecificada:faixasCepPrecificadas){
+            if(valorCarrinhoComDescontos>=faixaCepPrecificada.getMinValueToApply() && this.isCepBetween(shippingCalculationRequest.getCep(),faixaCepPrecificada.getCepInicio(),faixaCepPrecificada.getCepFim())){
+                return new FreteResponseDTO(
+                  faixaCepPrecificada.getValorFixo(),
+                  faixaCepPrecificada.getPrazo()
+                );
+            }
+        }
+        for(ShippingRules faixaCepNaoPrecificada:faixasCepNaoPrecificadas){
+            if(this.isCepBetween(shippingCalculationRequest.getCep(),faixaCepNaoPrecificada.getCepInicio(),faixaCepNaoPrecificada.getCepFim())){
+                return new FreteResponseDTO(
+                        faixaCepNaoPrecificada.getValorFixo(),
+                        faixaCepNaoPrecificada.getPrazo()
+                );
+            }
+        }
+        return this.getFreteCorreios(shippingCalculationRequest,franquia);
+    }
 
 
     public FreteResponseDTO getFreteCorreios(
             ShippingCalculationRequest shippingCalculationRequest
+            ,Franquia franquia
     ){
-        Franquia franquia = this.lojaRepository.findById(shippingCalculationRequest.getLojaId()).get().getFranquia();
         Optional<CorreiosFranquiaContext> correiosFranquiaContextOptional = this.correiosFranquiaRepository.findByFranquiaId(franquia.getSystemId());
         if(correiosFranquiaContextOptional.isEmpty()){
             throw new RuntimeException("Não foi possível calcular o frete, integração com correios não definida");
@@ -123,6 +162,20 @@ public class ShippingCalculator {
         );
     }
 
+    private ShippingDiscountable mapToDiscountable(
+            ShippingCalculationRequest shippingCalculationRequest,
+            Loja lojaEntity
+    ){//calcular o valor minimo do pedido para as faixas de cep, preciso aplicar os possiveis descontos
+        ShippingDiscountable output = new ShippingDiscountable();
+        LojaPedidoDTO lojaPedidoDTO = new LojaPedidoDTO();
+        lojaPedidoDTO.setSystemId(lojaEntity.getSystemId());
+        lojaPedidoDTO.setNome(lojaEntity.getNome());
+        lojaPedidoDTO.setSlug(lojaEntity.getSlug());
+        output.setLoja(lojaPedidoDTO);
+        output.setProdutos(shippingCalculationRequest.getProdutos());
+        return output;
+    }
+
     private Float getPesoCubado(
             Float comprimento,
             Float altura,
@@ -133,5 +186,9 @@ public class ShippingCalculator {
 
         // Arredondamento para 2 casas decimais
         return Math.round(pesoCubado * 100.0) / 100.0f;
+    }
+
+    private Boolean isCepBetween(String cep,String cepInferior,String cepSuperior){
+        return cep.compareTo(cepInferior) >= 0 && cep.compareTo(cepSuperior) <=0;
     }
 }
--- backend/src/main/java/com/skyler/catalogo/domain/shipping/ShippingDiscountable.java
+++ backend/src/main/java/com/skyler/catalogo/domain/shipping/ShippingDiscountable.java
@@ -1,20 +1,16 @@
 package com.skyler.catalogo.domain.shipping;
 
 import com.skyler.catalogo.domain.descontos.interfaces.Discountable;
+import com.skyler.catalogo.domain.lojas.Loja;
 import com.skyler.catalogo.domain.pedidos.DTOs.LojaPedidoDTO;
 import com.skyler.catalogo.domain.pedidos.DTOs.ProdutoPedidoDTO;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class ShippingCalculationRequest {
-    private String cep;
-    private String lojaId;
-    private List<ProdutoPedidoDTO> produtos = new ArrayList<>();
-    public void addProdutoComprado(ProdutoPedidoDTO produto){
-        this.produtos.add(produto);
-    }
-
+public class ShippingDiscountable implements Discountable {
+    List<ProdutoPedidoDTO> produtos;
+    LojaPedidoDTO loja;
+    Float valorFrete = 0f;
 }
